d784cdad6b7aecc19b1461ca17cb96de28b19bad
Carmen Guevara <carmen.guevara75@gmail.com>
2022-01-25 12:30:44 -0500

    Adding a User - hardcoded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
-import './App.css';
+import { useContext } from 'react'
 import Nav from './components/Nav';
+//contexts
+import UserContext from './contexts/UserContext';
+
+import './App.css';
 
 function App() {
+  //in able to use our context we import first then we can use the usecontext hook to access out context
+  // const user = useContext(UserContext);
+  // console.log(user)
   return (
     <div className="App">
-        <Nav/>
+      {/* {/* all context comes with the provider component This allows us to use this as a wrapper and share information to all of its children. after hardcoding the value (example) we need the value prop inside our provider. *} */}
+      <UserContext.Provider value={'Christina'}>
+        <Nav />
+      </UserContext.Provider>
     </div>
   );
 }
