14ce92bcd3e4915501bfd8c93eec3b933d74ee87
Carmen Guevara <carmen.guevara75@gmail.com>
2022-01-25 14:18:01 -0500

    creating Login Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { useContext } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Login from './pages/Login'
 //contexts
 import UserContext from './contexts/UserContext';
 
@@ -15,6 +17,11 @@ function App() {
       {/* {/* all context comes with the provider component This allows us to use this as a wrapper and share information to all of its children. after hardcoding the value (example) we need the value prop inside our provider. *} */}
       <UserContext.Provider value={'Carmen'}>
         <Nav />
+  
+        {/* We need to wrap all of our routes inside React Routes component  */}
+        <Routes>
+            <Route path='login' element={<Login/>} />
+        </Routes>
       </UserContext.Provider>
     </div>
   );
